2a29eea5b84acdc6cac6fbbec828da7ae7631ec8
Joe Schafranek <42823626+Jschafranek@users.noreply.github.com>
2021-03-09 14:40:25 -0700

    updated files

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = Vue.createApp({
         return{
             isFlipped: false,
             showAnswer: false,
-            questions: [
-                { question:`What is 76 - 33?`, 
-                  answer:`43`, }
-            ]
+            quiz: [
+                {question:`What is 76 - 33?`, answer:`43`, display: true},
+                {question:`How many legs does a dog have?`, answer:`4`, display: true },
+                {question:`What city do we live in?`, answer:`Gilbert`, display: true },
+                {question:`If you have 4 quarters, how many cents do you have?`, answer:`100`, display: true },
+                {question:`How many cents are in $1`, answer:`100`, display: true },
+                {question:`What is 208 + 54?`, answer:`262`, display: true },
+            ],
+            chosenSet: {}
         }
     },
 
@@ -19,6 +24,19 @@ const app = Vue.createApp({
         },
         toggleFlip() {
             this.isFlipped = !this.isFlipped
+        },
+        randomSet(){
+            var chosenNumber = Math.floor(Math.random() * this.quiz.length);
+            return this.chosenSet = this.quiz[chosenNumber];
+        },
+        markRead(){
+            this.chosenSet.display = false
+            console.log(this.chosenSet)
+        }
+    },
+    computed: {
+        filteredSet(){
+            return this.chosenSet = this.chosenSet.filter((quiz) => quiz.display)
         }
     }
 })
